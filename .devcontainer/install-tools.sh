7280f49a521d8d1803ed7fb1a5648121668f4616
Raynor Vliegendhart <raynor.vliegendhart@kadaster.nl>
2025-01-15 08:52:25 +0000

    Patch in additional type hints for pydapper

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -3,5 +3,18 @@
 # Install dev dependencies:
 python3 -m pip install qgis-stubs
 
+# Patch in additional type hints
+cat << EOF | sed -i '/class Commands(BaseCommands, ABC)/r /dev/stdin' /usr/local/lib/python3.12/dist-packages/pydapper/commands.py
+    
+    # begin type hint patch
+    @overload
+    def query(
+        self, sql: str, param: Optional["ParamType"] = ..., buffered: "Literal[True]" = True, *, model: Callable[..., "_T"]
+    ) -> List["_T"]:
+        ...
+    # end type hint patch
+
+EOF
+
 # Install any additional tools, e.g.:
 # apt update && apt install -y iputils-ping curl
