1cf5368e06c9a7a9af7377500e1af9f5e692b52a
Raynor Vliegendhart <raynor.vliegendhart@kadaster.nl>
2025-01-28 13:08:21 +0100

    Revert "Update pydapper to 0.11.1 and remove obsolete patch"
    This reverts commit 223128c01b3786133b68012cb26537521badaab5.
    
    Package pydapper 0.11.1 seems to cause infinite recursion
    when attempting to connect to a Postgres database?

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -4,6 +4,18 @@
 python3 -m pip install qgis-stubs
 
 # Patch in additional type hints
+cat << EOF | sed -i '/class Commands(BaseCommands, ABC)/r /dev/stdin' /usr/local/lib/python3.12/dist-packages/pydapper/commands.py
+    
+    # begin type hint patch
+    @overload
+    def query(
+        self, sql: str, param: Optional["ParamType"] = ..., buffered: "Literal[True]" = True, *, model: Callable[..., "_T"]
+    ) -> List["_T"]:
+        ...
+    # end type hint patch
+
+EOF
+
 cat << EOF | sed -i '/class ConnectionType/r /dev/stdin' /usr/local/lib/python3.12/dist-packages/pydapper/types.py
 
     # begin type hint patch
