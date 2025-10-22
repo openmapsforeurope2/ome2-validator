1eb6f8a79bd4efd30ff53c5c6c5d66eb7f493e99
Raynor Vliegendhart <raynor.vliegendhart@kadaster.nl>
2025-10-22 13:08:40 +0000

    Add support for wildcards in AllowedAttributeValidator

--- validators/allowed_attribute_validator.py
+++ validators/allowed_attribute_validator.py
@@ -1,3 +1,4 @@
+from fnmatch import fnmatch
 from qgis.core import QgsFeatureRequest, QgsVectorLayer, QgsFeature
 from models import ValidationResult
 from . import FeatureValidator
@@ -25,8 +26,6 @@ class AllowedAttributeValidator(FeatureValidator):
 
         Returns:
             list[ValidationResult]: A list of results, containing the features of which the specified field does not have an allowed value.
-
-        # TODO Support wildcard values such as 'void_*'
         """
         results = []
 
@@ -34,10 +33,23 @@ class AllowedAttributeValidator(FeatureValidator):
             cls.logger.warning(f"Cannot run the {cls.__name__} on {feature_class.name()} for field {field_name} because the field does not exist.")
             return results
         
-        allowed_attributes_quoted = [f"\'{attr}\'" for attr in allowed_attributes]
+        allowed_values, allowed_patterns = cls.get_allowed_values_and_patterns(allowed_attributes)
+
+        allowed_attributes_quoted = [f"\'{attr}\'" for attr in allowed_values]
         allowed_attributes_list = f"({','.join(allowed_attributes_quoted)})"
         
         filter_expression = f'"{field_name}" not in {allowed_attributes_list}'
+        
+        if allowed_patterns:
+            like_expressions = (
+                f'"{field_name}" not like {pat}'
+                for pat in allowed_patterns
+            )
+            filter_expression = (
+                filter_expression 
+                + ' and '
+                + ' and '.join(like_expressions)
+            )
 
         for feature in feature_class.getFeatures(QgsFeatureRequest().setFilterExpression(filter_expression)):
             field_value = feature.attribute(field_name)
@@ -46,8 +58,12 @@ class AllowedAttributeValidator(FeatureValidator):
             country = cls.get_attribute(feature, 'country')
             if separator is not None and separator in field_value:
                 split_values = field_value.split(separator)
-                not_allowed = [val for val in split_values if val not in allowed_attributes]
-
+                not_allowed = [
+                    val for val in split_values 
+                    if val not in allowed_values
+                    and not any(fnmatch(val, pat) for pat in allowed_patterns)
+                ]
+                
                 if len(not_allowed) > 0:
                     # Create error message for invalid values combined by a separator
                     message = cls.create_invalid_value_message(feature_class, feature, field_name)
@@ -97,6 +113,19 @@ class AllowedAttributeValidator(FeatureValidator):
                 attributes.append(value)
 
         return attributes, patterns
+    
+    @classmethod 
+    def quote_pattern(cls, allowed_pattern: str) -> str:
+        """Quotes a pattern such that it can be used in a filter expression.
+        It escapes underscores and replaces the * wildcard symbol with %.
+
+        Args:
+            allowed_pattern (str): an allowed attribute pattern.
+
+        Returns:
+            str: the quoted pattern.
+        """
+        return allowed_pattern.replace('_', r'\_').replace('*', '%')
         
 
     @classmethod
